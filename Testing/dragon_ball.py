774dcc343011c9883c6cc0e3cd3efb806c711a7d
biyonimo <yoni.meirov08@gmail.com>
2021-01-23 21:14:56 +0200

    dragon ball abilities - char 4,5,6

--- Testing/dragon_ball.py
+++ Testing/dragon_ball.py
@@ -2,20 +2,21 @@ from pprint import pprint
 
 
 class DBCharacter:
-    def __init__(self, short_name, id, evolution_name, tags, ability_bonus):
+    def __init__(self, short_name, id, evolution_name, tags, ability_bonus, color):
         self.short_name = short_name
         self.evolution_name = evolution_name
         self.ability_bonus = ability_bonus
         self.id = id
         self.tags = []
         self.tags += tags
+        self.color = color
 
 
 class DBGodKi (DBCharacter):
-    def __init__(self, short_name, id, evolution_name, tags, ability_bonus):
+    def __init__(self, short_name, id, evolution_name, tags, ability_bonus, color):
         tags_ki = ["God Ki"]
         tags_ki += tags
-        DBCharacter.__init__(self, short_name, id, evolution_name, tags_ki, ability_bonus)
+        DBCharacter.__init__(self, short_name, id, evolution_name, tags_ki, ability_bonus, color)
 
 
 def calc_ability_bonus_two(char1, char2):
@@ -38,13 +39,15 @@ if __name__ == "__main__":
                           "DBL13-01S",
                           "Super Saiyan God SS",
                           tags={"Saiyan", "Son Family"},
-                          ability_bonus=23*2)
+                          ability_bonus=23*2,
+                          color="Blue")
 
     character_2 = DBGodKi("Goku Black",
                           "DBL18-06S",
                           "Super Saiyan Rose",
-                          {"Saiyan", "Future", "Powerful opponent"},
-                          ability_bonus=19)
+                           tags={"Saiyan", "Future", "Powerful opponent"},
+                          ability_bonus=19,
+                          color="red")
 
     characters = (character_1, character_2)
     ability_bonus = calc_ability_bonus(characters)
@@ -53,21 +56,40 @@ if __name__ == "__main__":
     character_3 = DBGodKi("Goku",
                           "DBL20-05S",
                           "Super Saiyan God",
-                          {"Saiyan", "Son Family"},
-                          ability_bonus=19)
+                          tags={"Saiyan", "Son Family"},
+                          ability_bonus=19,
+                          color="purple")
+    character_4 = DBGodKi("beerus",
+                          "dbl07 11s",
+                          "God of Destruction",
+                          tags={"Powerful ", "Twins"},
+                          ability_bonus=19,
+                          color="green")
 
-    characters = (character_1, character_2, character_3)
+    character_5 = DBGodKi("beerus",
+                          "dbl07 11s",
+                          "God of Destruction",
+                          tags={"Powerful ", "Twins"},
+                          ability_bonus=23,
+                          color="yellow")
+
+    character_6 = DBGodKi("Goku",
+                          "DBL15-07S",
+                          "Super Saiyan God SS",
+                          tags={"Saiyan", "Son Family"},
+                          ability_bonus=24,
+                          color="Yellow")
+
+    characters = (character_1,character_2,character_3,character_4, character_5, character_6)  # , character_5,)
     ability_bonus = calc_ability_bonus(characters)
     print(f"Ability bonus: {ability_bonus}")
 
-      # character_4 = DBCharacter("Goku",
-    #                           "Super Saiyan God SS",
-    #                           {"god_ki", "Saiyan", "Son Family"})
-    #
-    # character_5 = DBCharacter("Goku",
-    #                           "Super Saiyan God SS",
-    #                           {"god_ki", "Saiyan", "Son Family"})
-    #
+    # characters = (character_1, character_2, character_3, character_4 ) #, character_5,)
+    # ability_bonus = calc_ability_bonus(characters)
+    # print(f"Ability bonus: {ability_bonus}")
+
+
+
     # character_6 = DBCharacter("Goku",
     #                           "Super Saiyan God SS",
     #                           {"god_ki", "Saiyan", "Son Family"})
